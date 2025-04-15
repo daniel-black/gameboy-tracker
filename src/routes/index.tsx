a02c0046279dbadd0491e2accd0fd7ffbc14a039
daniel-black <drblack651@gmail.com>
2025-04-15 06:35:17 -0600

    forgot stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,10 @@
 // import { App } from "@/App";
+import { ActivePanel } from "@/components/active-panel";
 import { Editor } from "@/components/editor";
+import { GlobalControls } from "@/components/global-controls";
+import { PatternManager } from "@/components/pattern-manager";
+import { Playback } from "@/components/playback";
+import { SongManager } from "@/components/song-manager";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -7,5 +12,22 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
-  return <Editor />; // This is the main app component
+  return (
+    <div className="h-screen w-full flex flex-row gap-4 p-4 overflow-hidden">
+      <div className="h-full">
+        <Editor />
+      </div>
+      <div className="flex-1 flex flex-row gap-4">
+        <div className="flex-1 flex flex-col gap-4">
+          <Playback />
+          <GlobalControls />
+          <ActivePanel />
+        </div>
+        <div className="flex flex-col gap-4 min-w-44">
+          <PatternManager />
+          <SongManager />
+        </div>
+      </div>
+    </div>
+  );
 }
